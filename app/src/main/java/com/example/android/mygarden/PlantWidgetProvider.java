b3f560acb98b41330ab08b6684cf7a91d9e537d3
Edward <ed828a@gmail.com>
2017-11-13 20:09:15 +1030

    complete task 2

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -13,7 +13,8 @@ import com.example.android.mygarden.ui.MainActivity;
  * Implementation of App Widget functionality.
  */
 public class PlantWidgetProvider extends AppWidgetProvider {
-    public static final int REQUEST_CODE = 0;
+    public static final int PLANT_REQUEST_CODE = 0;
+    public static final int WATER_REQUEST_CODE = 1;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -24,10 +25,17 @@ public class PlantWidgetProvider extends AppWidgetProvider {
 
         // Create an Intent to launch MainActivity when clicked.
         Intent intent = new Intent(context, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, REQUEST_CODE, intent, 0);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, PLANT_REQUEST_CODE, intent, 0);
 
         // Widgets allow click handlers to only launch pending intents
         views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+
+        Intent startServiceIntent = new Intent(context, PlantWateringService.class);
+        startServiceIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
+        PendingIntent wateringPendingIntent = PendingIntent.getService(context,
+                WATER_REQUEST_CODE, startServiceIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.widget_water_button, wateringPendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -39,6 +47,11 @@ public class PlantWidgetProvider extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // Perform any action when one or more Appwidget instances have been deleted
     }
 
     @Override
